b126e6f84b7cddb0df34160bbdb1e3e188893694
Chitroopa Manikkavasagam, Marcel Nogueira and Starlynne Start <marcel@>
2017-03-29 09:16:18 -0700

    Add to scripts rule for spec 3, test, spec 3 logic valid

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 var leapYear = function(year) {
-  if (year % 4 === 0)
+  if ((year % 4 === 0) && (year % 100 !== 0))
   {
     return true;
   }
   else
   {
-  return false;
+   return false;
   }
 };
 
